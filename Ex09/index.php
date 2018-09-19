5eaa6e7ac0ba1dbd5cf077ec76b825e7727fb2ba
Alison Moura <alison.oghino@gmail.com>
2018-09-19 19:43:19 -0400

    Solução Ex09

---
+++ Ex09/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+* Material utilizado para as aulas práticas da disciplinas da Faculdade de
+* Computação da Universidade Federal de Mato Grosso do Sul (FACOM / UFMS).
+* Seu uso é permitido para fins apenas acadêmicos, todavia mantendo a
+* referência de autoria.
+* @author Profª Jane Eleutério
+
+Página para cadastro de novo contato na agenda.
+-->
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Agenda de Contatos</title>
+    <link rel="stylesheet" type="text/css" href="estilo.css"/>
+</head>
+<body>
+    <h1>Agenda de Contatos</h1>
+    <p>
+        <a href="gerenciador.php?funcao=novo">Novo</a>
+        <a href="gerenciador.php?funcao=listar">Listar</a>
+        <a href="gerenciador.php?funcao=limpar">Limpar</a>
+    </p>
+</body>
+</html>
