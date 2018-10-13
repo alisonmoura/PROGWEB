387fb35c1fe4eb34a08e955bcc0f5b1722331a8e
Alison Moura <alison.oghino@gmail.com>
2018-10-13 15:23:00 -0400

    Coonsertando tipo integer para decumal

---
+++ ExConversorDeMedidas/sql/create_convertion_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE convertion(
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
+    unity_from VARCHAR NOT NULL,
+    unity_to VARCHAR NOT NULL,
+    convertion_value DECIMAL NOT NULL
+
+)
--- ExConversorDeMedidas/sql/create_historic_table.sql
+++ ExConversorDeMedidas/sql/create_historic_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE historic(
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
-    value_from INTEGER NOT NULL,
+    value_from DECIMAL NOT NULL,
     unity_from VARCHAR NOT NULL,
-    value_to INTEGER NOT NULL,
+    value_to DECIMAL NOT NULL,
     unity_to VARCHAR NOT NULL
 )
